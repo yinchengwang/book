94968b3858d23280e4c812d1c5af39f27ffa154b
yinchengwang <yinchengwang@hotmail.com>
2026-09-02 09:43:33 +0800

    fix(storage): rel.c 添加 fcntl.h 和 unistd.h (Windows build fix)

--- engineering/src/db/storage/rel/rel.c
+++ engineering/src/db/storage/rel/rel.c
@@ -11,6 +11,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <fcntl.h>
+#ifdef _WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
 
 /* ============================================================
  * 全局状态
